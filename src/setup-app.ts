ad0079d1363f49bcdbae1a1390e09775f9112c45
Alexander <sasha.kozel252000@gmail.com>
2025-12-01 17:17:22 +0300

    commit

---
+++ src/setup-app.ts
@@ -0,0 +1,20 @@
+import express, {Express, Request, Response} from "express";
+import {BLOGS_PATH, POSTS_PATH, TESTING_PATH} from "./core/paths/paths";
+import {blogsRouter} from "./blogs/router/blogs-router";
+import {testingRouter} from "./testing/router/testing-router";
+import {postsRouter} from "./posts/router/postsRouter";
+
+
+export const setupApp = (app: Express) => {
+
+    app.use(express.json());
+
+    app.get('/', (req: Request, res: Response) => {
+        res.json({message: 'Hello World!'});
+    })
+    app.use(BLOGS_PATH, blogsRouter);
+    app.use(POSTS_PATH, postsRouter);
+    app.use(TESTING_PATH, testingRouter);
+
+    return app;
+}
