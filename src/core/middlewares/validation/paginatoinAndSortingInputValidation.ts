0fcc45ed4833d89c467da09ed159c3f2c58a15e0
Alexander <sasha.kozel252000@gmail.com>
2025-12-01 17:17:15 +0300

    commit

---
+++ src/core/middlewares/validation/paginatoinAndSortingInputValidation.ts
@@ -0,0 +1,26 @@
+import {query} from 'express-validator';
+import express from "express";
+
+const DEFAULT_PAGE_NUMBER = 1;
+const DEFAULT_PAGE_SIZE = 10;
+
+enum SortByFields {
+
+}
+
+
+const pageNumberValidation = query('pageNumber')
+    .default(DEFAULT_PAGE_NUMBER)
+    .isInt({min: 1})
+    .withMessage('pageNumber must be a positive integer')
+
+const pageSizeValidation = query('pageSize')
+    .default(DEFAULT_PAGE_SIZE)
+    .isInt({min: 1})
+    .withMessage('pageSize must be a positive integer')
+
+export const paginationAndSortingInputValidation = [
+    pageNumberValidation,
+    pageSizeValidation,
+]
+
