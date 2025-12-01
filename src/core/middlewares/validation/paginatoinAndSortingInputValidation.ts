0ef867dbc47b8753a304f4419db872977c21cd03
Alexander <sasha.kozel252000@gmail.com>
2025-12-01 23:51:25 +0300

    commit

--- src/core/middlewares/validation/paginatoinAndSortingInputValidation.ts
+++ src/core/middlewares/validation/paginatoinAndSortingInputValidation.ts
@@ -2,24 +2,41 @@ import {query} from 'express-validator';
 
 const DEFAULT_PAGE_NUMBER = 1;
 const DEFAULT_PAGE_SIZE = 10;
+const DEFAULT_SORT_DIRECTION = 'desc';
 
-enum SortByFields {
-
+enum sortByFields {
+    createdAt = 'createdAt',
 }
 
-
 const pageNumberValidation = query('pageNumber')
+    .optional({ checkFalsy: true })
     .default(DEFAULT_PAGE_NUMBER)
     .isInt({min: 1})
     .withMessage('pageNumber must be a positive integer')
 
 const pageSizeValidation = query('pageSize')
+    .optional({ checkFalsy: true })
     .default(DEFAULT_PAGE_SIZE)
     .isInt({min: 1})
     .withMessage('pageSize must be a positive integer')
 
+const sortByValidation = query('sortBy')
+    .optional({ checkFalsy: true })
+    .default(Object.values(sortByFields)[0])
+    .isIn(Object.values(sortByFields))
+    .withMessage('sortBy invalid value');
+
+const sortDirectionValidation = query('sortDirection')
+    .optional({ checkFalsy: true })
+    .default(DEFAULT_SORT_DIRECTION)
+    .isIn(['desc', 'asc'])
+    .withMessage('sortDirection must be asc or desc')
+
+
 export const paginationAndSortingInputValidation = [
     pageNumberValidation,
     pageSizeValidation,
+    sortByValidation,
+    sortDirectionValidation,
 ]
 
--- src/posts/router/handlers/getPostHandler.ts
+++ src/posts/router/handlers/getPostHandler.ts
@@ -1,9 +1,10 @@
 import {Request, Response} from 'express';
 import {postsRepository} from "../../repositories/postsRepository";
 import {mapPostToViewModel} from "../../mapers/mapPostToViewModel";
+import {postsService} from "../../application/postsService";
 
 export const getPostHandler = async (req: Request, res: Response) => {
-    const foundPost = await postsRepository.findById(req.params.id);
+    const foundPost = await postsService.findById(req.params.id);
     if (!foundPost) {
         res.sendStatus(404);
         return;
