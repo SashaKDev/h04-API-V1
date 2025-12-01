1de960f1fc57b0d432b0491dd5b7299f7632045a
Alexander <sasha.kozel252000@gmail.com>
2025-12-01 17:17:19 +0300

    commit

---
+++ src/posts/router/postsRouter.ts
@@ -0,0 +1,38 @@
+import {Router} from "express";
+import {basicAuthMiddleware} from "../../auth/middlewares/basicAuthMiddleware";
+import {postInputDtoValidation} from "../validation/postInputDtoValidation";
+import {inputValidationResult} from "../../core/middlewares/validation/inputValidationResult";
+import {idValidation} from "../../core/middlewares/validation/paramValidation";
+import {getAllPostsHandler} from "./handlers/getAllPostsHandler";
+import {getPostHandler} from "./handlers/getPostHandler";
+import {createPostHandler} from "./handlers/createPostHandler";
+import {updatePostHandler} from "./handlers/updatePostHandler";
+import {deletePostHandler} from "./handlers/deletePostHandler";
+
+export const postsRouter = Router({});
+
+postsRouter.get('/', getAllPostsHandler);
+
+postsRouter.get('/:id', getPostHandler);
+
+postsRouter.post('/',
+    basicAuthMiddleware,
+    postInputDtoValidation,
+    inputValidationResult,
+    createPostHandler
+);
+
+postsRouter.put('/:id',
+    basicAuthMiddleware,
+    idValidation,
+    postInputDtoValidation,
+    inputValidationResult,
+    updatePostHandler
+);
+
+postsRouter.delete('/:id',
+    basicAuthMiddleware,
+    idValidation,
+    inputValidationResult,
+    deletePostHandler
+);
