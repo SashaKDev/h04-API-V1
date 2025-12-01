2062e8b7ac2aa46ce40d1aa1635b0acfddd691e1
Alexander <sasha.kozel252000@gmail.com>
2025-12-01 23:51:08 +0300

    commit

--- src/posts/router/handlers/createPostHandler.ts
+++ src/posts/router/handlers/createPostHandler.ts
@@ -1,24 +1,22 @@
 import {Request, Response} from 'express';
-import {Post} from "../../types/post";
-import {blogsRepository} from "../../../blogs/repositories/blogsRepository";
-import {postsRepository} from "../../repositories/postsRepository";
 import {mapPostToViewModel} from "../../mapers/mapPostToViewModel";
+import {PostInputDto} from "../../dto/post-input.dto";
+import {postsService} from "../../application/postsService";
+import {blogsService} from "../../../blogs/application/blogsService";
 
 export const createPostHandler = async (req: Request, res: Response) => {
-    const foundBlog = await blogsRepository.findById(req.body.blogId);
+    const foundBlog = await blogsService.findById(req.body.blogId);
     if (!foundBlog) {
         res.sendStatus(404);
         return;
     }
-    const newPost: Post = {
+    const newPost: PostInputDto = {
         title: req.body.title,
         shortDescription: req.body.shortDescription,
         content: req.body.content,
         blogId: req.body.blogId,
-        blogName: foundBlog.name,
-        createdAt: new Date().toISOString(),
     }
-    const createdPost = await postsRepository.create(newPost);
+    const createdPost = await postsService.create(newPost);
     res
         .status(201)
         .json(mapPostToViewModel(createdPost));
--- src/posts/router/handlers/deletePostHandler.ts
+++ src/posts/router/handlers/deletePostHandler.ts
@@ -1,13 +1,14 @@
 import {Request, Response} from 'express';
 import {postsRepository} from "../../repositories/postsRepository";
+import {postsService} from "../../application/postsService";
 
 export const deletePostHandler = async (req: Request, res: Response) => {
-    const foundPost = await postsRepository.findById(req.params.id);
+    const foundPost = await postsService.findById(req.params.id);
     if (!foundPost) {
         res.sendStatus(404);
         return;
     }
 
-    await postsRepository.delete(req.params.id);
+    await postsService.delete(req.params.id);
     res.sendStatus(204);
 }
