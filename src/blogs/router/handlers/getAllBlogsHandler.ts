a7d0403aed9d6177cbf3c69c6147faddb4abbffc
Alexander <sasha.kozel252000@gmail.com>
2025-12-02 21:42:33 +0300

    commit

--- src/blogs/router/handlers/getAllBlogsHandler.ts
+++ src/blogs/router/handlers/getAllBlogsHandler.ts
@@ -2,16 +2,17 @@ import {Request, Response} from 'express';
 import {blogsService} from "../../application/blogsService";
 import {mapToBlogsWithPaginator} from "../../mapers/mapToBlogsWithPaginator";
 import {matchedData} from "express-validator";
+import {getAllBlogsInputQueryDto} from "../../dto/getAllBlogsInputQueryDto";
 
 export const getAllBlogsHandler = async (req: Request, res: Response) => {
 
     const data = matchedData(req, { locations: ['query'] });
     console.log(data);
     const pageNumber = Number(data.pageNumber);
-    const pageSize = Number(data.ageSize);
-    const sortBy = req.query.sortBy as string;
-    const sortDirection = req.query.sortDirection as string;
-    const searchNameTerm = req.query.searchNameTerm as string;
+    const pageSize = Number(data.pageSize);
+    const sortBy = data.sortBy;
+    const sortDirection = data.sortDirection;
+    const searchNameTerm = data.searchNameTerm;
 
     const allBlogs = await blogsService.findAll(pageNumber, pageSize, sortBy, sortDirection, searchNameTerm)
     const allBlogsViewModel = mapToBlogsWithPaginator(allBlogs);
