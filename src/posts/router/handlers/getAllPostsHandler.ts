864e0e917e419cff2ecc61b6b98c240997df2e07
Alexander <sasha.kozel252000@gmail.com>
2025-12-02 21:42:35 +0300

    commit

--- src/posts/router/handlers/getAllPostsHandler.ts
+++ src/posts/router/handlers/getAllPostsHandler.ts
@@ -1,15 +1,22 @@
 import {Request, Response} from "express";
 import {mapPostToViewModel} from "../../mapers/mapPostToViewModel";
 import {postsService} from "../../application/postsService";
+import {matchedData} from "express-validator";
+import {mapToBlogPostsWithPaginator} from "../../../blogs/mapers/mapToBlogPostsWithPaginator";
 
 export const getAllPostsHandler = async (req: Request, res: Response) => {
 
-    const pageSize = Number(req.query.pageSize);
-    const pageNumber = Number(req.query.pageNumber);
+    const data = matchedData(req, { locations: ['query'] });
+    console.log(data);
+    const pageSize = Number(data.pageSize);
+    const pageNumber = Number(data.pageNumber);
+    const sortBy = data.sortBy;
+    const sortDirection = data.sortDirection;
 
-    const foundPosts = await postsService.findAll(pageSize, pageNumber);
-    const foundPostsViewModel = foundPosts.map(mapPostToViewModel);
+    const foundPosts = await postsService.findAll(pageSize, pageNumber, sortDirection, sortBy);
+    const foundPostsWithPaginator = mapToBlogPostsWithPaginator(foundPosts);
+
     res
         .status(200)
-        .json(foundPostsViewModel);
+        .json(foundPostsWithPaginator);
 }
