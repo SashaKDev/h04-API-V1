2e62af1e6eb5e10ea35e5a676c5508eba04157f6
Alexander <sasha.kozel252000@gmail.com>
2025-12-02 21:42:38 +0300

    commit

--- src/core/middlewares/validation/paginatoinAndSortingInputValidation.ts
+++ src/core/middlewares/validation/paginatoinAndSortingInputValidation.ts
@@ -6,31 +6,40 @@ const DEFAULT_SORT_DIRECTION = 'desc';
 
 enum sortByFields {
     createdAt = 'createdAt',
+    name = 'name',
+    description = 'description',
+    websiteUrl = 'websiteUrl',
+    id = 'id',
+    title = 'title',
+    shortDescription = 'shortDescription',
+    content = 'content',
+    blogId = 'blogId',
+    blogName = 'blogName',
+
 }
 
 const pageNumberValidation = query('pageNumber')
-    .optional({ checkFalsy: true })
     .default(DEFAULT_PAGE_NUMBER)
     .isInt({min: 1})
     .withMessage('pageNumber must be a positive integer')
 
 const pageSizeValidation = query('pageSize')
-    .optional({ checkFalsy: true })
     .default(DEFAULT_PAGE_SIZE)
     .isInt({min: 1})
     .withMessage('pageSize must be a positive integer')
 
 const sortByValidation = query('sortBy')
-    .optional({ checkFalsy: true })
     .default(Object.values(sortByFields)[0])
     .isIn(Object.values(sortByFields))
     .withMessage('sortBy invalid value');
 
 const sortDirectionValidation = query('sortDirection')
-    .optional({ checkFalsy: true })
     .default(DEFAULT_SORT_DIRECTION)
     .isIn(['desc', 'asc'])
     .withMessage('sortDirection must be asc or desc')
+
+const searchNameTermValidation = query('searchNameTerm')
+    .default(null)
 
 
 export const paginationAndSortingInputValidation = [
@@ -38,5 +47,6 @@ export const paginationAndSortingInputValidation = [
     pageSizeValidation,
     sortByValidation,
     sortDirectionValidation,
+    searchNameTermValidation
 ]
 
