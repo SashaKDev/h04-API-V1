228ac27964000936f3a29a0ec0153f095032453b
Alexander <sasha.kozel252000@gmail.com>
2025-12-05 22:46:58 +0300

    CQRS updates

--- src/posts/router/handlers/deletePostHandler.ts
+++ src/posts/router/handlers/deletePostHandler.ts
@@ -2,12 +2,12 @@ import {Request, Response} from 'express';
 import {postsService} from "../../application/postsService";
 
 export const deletePostHandler = async (req: Request, res: Response) => {
-    const foundPost = await postsService.findById(req.params.id);
-    if (!foundPost) {
+
+    const deleteResult: number = await postsService.delete(req.params.id);
+    if (!deleteResult) {
         res.sendStatus(404);
         return;
     }
-
-    await postsService.delete(req.params.id);
     res.sendStatus(204);
+
 }
