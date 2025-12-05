f3c03cd9c613375942bd09a9c4919d8736115611
Alexander <sasha.kozel252000@gmail.com>
2025-12-05 22:46:55 +0300

    CQRS updates

--- src/blogs/application/blogsService.ts
+++ src/blogs/application/blogsService.ts
@@ -2,41 +2,45 @@ import {blogsRepository} from "../repositories/blogsRepository";
 import {Blog} from "../types/blog";
 import {WithId} from "mongodb";
 import {BlogInputDto} from "../dto/blog-input.dto";
-import {BlogWithPaginator} from "../types/blogWithPaginator";
+import {BlogViewModel} from "../types/blogsViewModel";
+import {mapBlogToViewModel} from "../mapers/mapBlogToViewModel";
+import {blogsQueryRepository} from "../repositories/blogsQueryRepository";
 
 
 export const blogsService = {
-    async findAll(pageNumber: number, pageSize: number, sortBy: string, sortDirection: string, searchNameTerm: string): Promise<BlogWithPaginator> {
-        const skip = (pageNumber - 1) * pageSize;
-        const limit = pageSize
-        const foundBlogs = await blogsRepository.findAll(skip, limit, sortBy, sortDirection, searchNameTerm);
-        const pagesCount = Math.ceil(foundBlogs.totalCount / pageSize)
-        return {
-            ...foundBlogs,
-            pageSize: pageSize,
-            page: pageNumber,
-            pagesCount: pagesCount,
-        }
-    },
+    // async findAll(pageNumber: number, pageSize: number, sortBy: string, sortDirection: string, searchNameTerm: string): Promise<BlogsWithPaginator> {
+    //     const skip = (pageNumber - 1) * pageSize;
+    //     const limit = pageSize
+    //     const foundBlogs = await blogsRepository.findAll(skip, limit, sortBy, sortDirection, searchNameTerm);
+    //     const pagesCount = Math.ceil(foundBlogs.totalCount / pageSize)
+    //     return {
+    //         ...foundBlogs,
+    //         pageSize: pageSize,
+    //         page: pageNumber,
+    //         pagesCount: pagesCount,
+    //     }
+    // },
 
     async findById(id: string): Promise<WithId<Blog> | null> {
         return await blogsRepository.findById(id);
     },
 
-    async create(blog: BlogInputDto): Promise<WithId<Blog>> {
+    async create(blog: BlogInputDto): Promise<string> {
         const newBlog = {
-            ...blog,
+            name: blog.name,
+            description: blog.description,
+            websiteUrl: blog.websiteUrl,
             createdAt: new Date().toISOString(),
             isMembership: false
         };
         return await blogsRepository.create(newBlog);
     },
 
-    async update(id: string, blog: BlogInputDto): Promise<void> {
-        await blogsRepository.update(id, blog);
+    async update(id: string, blog: BlogInputDto): Promise<number> {
+        return await blogsRepository.update(id, blog);
     },
 
-    async delete(id: string): Promise<void> {
-        await blogsRepository.delete(id);
+    async delete(id: string): Promise<number> {
+        return await blogsRepository.delete(id);
     }
 }
--- src/blogs/router/handlers/createBlogHandler.ts
+++ src/blogs/router/handlers/createBlogHandler.ts
@@ -1,24 +1,21 @@
 import {Request, Response} from 'express';
-import {mapBlogToViewModel} from "../../mapers/mapBlogToViewModel";
 import {BlogInputDto} from "../../dto/blog-input.dto";
 import {blogsService} from "../../application/blogsService";
+import {blogsQueryRepository} from "../../repositories/blogsQueryRepository";
 
 export const createBlogHandler = async (req: Request, res: Response) => {
-    try {
-        const newBlog: BlogInputDto = {
-            name: req.body.name,
-            description: req.body.description,
-            websiteUrl: req.body.websiteUrl,
-        }
 
-        const createdBlog = await blogsService.create(newBlog);
-        const createdBlogViewModel = mapBlogToViewModel(createdBlog);
-
-        res
-            .status(201)
-            .json(createdBlogViewModel);
-
-    } catch (error) {
-        res.sendStatus(500);
+    const newBlog: BlogInputDto = {
+        name: req.body.name,
+        description: req.body.description,
+        websiteUrl: req.body.websiteUrl,
     }
+
+    const createdBlogId = await blogsService.create(newBlog);
+    const createdBlogViewModel = await blogsQueryRepository.findById(createdBlogId);
+
+    res
+        .status(201)
+        .json(createdBlogViewModel);
+
 }
