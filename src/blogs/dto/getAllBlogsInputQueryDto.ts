f2e9d15267015ec59eda6da3bfba43cbc8e92e52
Alexander <sasha.kozel252000@gmail.com>
2025-12-05 22:47:01 +0300

    CQRS updates

--- src/blogs/dto/getAllBlogsInputQueryDto.ts
+++ src/blogs/dto/getAllBlogsInputQueryDto.ts
@@ -1,5 +1,3 @@
-
-
 export type getAllBlogsInputQueryDto = {
     pageNumber: number,
     pageSize: number,
--- src/posts/router/handlers/getAllPostsHandler.ts
+++ src/posts/router/handlers/getAllPostsHandler.ts
@@ -1,19 +1,17 @@
 import {Request, Response} from "express";
-import {postsService} from "../../application/postsService";
 import {matchedData} from "express-validator";
-import {mapToBlogPostsWithPaginator} from "../../../blogs/mapers/mapToBlogPostsWithPaginator";
+import {postsQueryRepository} from "../../repositories/postsQueryRepository";
 
 export const getAllPostsHandler = async (req: Request, res: Response) => {
 
     const data = matchedData(req, { locations: ['query'] });
-    console.log(data);
+
     const pageSize = Number(data.pageSize);
     const pageNumber = Number(data.pageNumber);
     const sortBy = data.sortBy;
     const sortDirection = data.sortDirection;
 
-    const foundPosts = await postsService.findAll(pageSize, pageNumber, sortDirection, sortBy);
-    const foundPostsWithPaginator = mapToBlogPostsWithPaginator(foundPosts);
+    const foundPostsWithPaginator = await postsQueryRepository.findAll(pageSize, pageNumber, sortDirection, sortBy);
 
     res
         .status(200)
