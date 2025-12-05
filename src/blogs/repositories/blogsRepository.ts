c018d2b74878acb6012d76ddbf13d76e71d626f6
Alexander <sasha.kozel252000@gmail.com>
2025-12-05 22:47:13 +0300

    CQRS updates

--- src/blogs/repositories/blogsRepository.ts
+++ src/blogs/repositories/blogsRepository.ts
@@ -5,36 +5,18 @@ import {blogsCollection} from "../../db/mongo.db";
 
 
 export const blogsRepository = {
-    async findAll(skip: number, limit: number, sortBy: string, sortDirection: string, searchNameTerm: string): Promise<{items: WithId<Blog>[], totalCount: number}>{
-        const sortDirectionNumber = sortDirection === 'desc' ? -1 : 1;
-        console.log(sortBy)
-        let filter = {};
-        if (searchNameTerm) {
-            filter = { name: { $regex: searchNameTerm, $options: "i" } }
-        }
-        return  {
-            items:
-                await blogsCollection
-                .find(filter)
-                .sort({ [sortBy]:  sortDirectionNumber})
-                .skip(skip)
-                .limit(limit)
-                .toArray(),
-            totalCount: await blogsCollection.countDocuments(filter)
-        }
-    },
 
-    async findById(id: string): Promise<WithId<Blog> | null>{
+    async findById(id: string): Promise<WithId<Blog> | null> {
         return await blogsCollection.findOne({_id: new ObjectId(id)});
     },
 
-    async create(blog: Blog): Promise<WithId<Blog>> {
+    async create(blog: Blog): Promise<string> {
         const insertResult = await blogsCollection.insertOne(blog);
-        return {...blog, _id: insertResult.insertedId}
+        return insertResult.insertedId.toString();
 
     },
 
-    async update(id: string, dto: BlogInputDto): Promise<void> {
+    async update(id: string, dto: BlogInputDto): Promise<number> {
         const updateResult = await blogsCollection.updateOne(
             {_id: new ObjectId(id)},
             {$set:
@@ -45,15 +27,11 @@ export const blogsRepository = {
                     }
             }
             );
-        if (updateResult.matchedCount === 0) {
-            throw new Error("No blogs found.");
-        }
+        return updateResult.matchedCount;
     },
 
-    async delete(id: string): Promise<void> {
+    async delete(id: string): Promise<number> {
         const deleteResult = await blogsCollection.deleteOne({_id: new ObjectId(id)});
-        if (deleteResult.deletedCount === 0) {
-            throw new Error("No blogs found.");
-        }
+        return deleteResult.deletedCount;
     },
 }
