43a5999c093ceea26ea8b1bbd4f232dbcff5a433
Alexander <sasha.kozel252000@gmail.com>
2025-12-05 22:46:52 +0300

    CQRS updates

--- src/posts/router/handlers/createPostHandler.ts
+++ src/posts/router/handlers/createPostHandler.ts
@@ -3,21 +3,23 @@ import {mapPostToViewModel} from "../../mapers/mapPostToViewModel";
 import {PostInputDto} from "../../dto/post-input.dto";
 import {postsService} from "../../application/postsService";
 import {blogsService} from "../../../blogs/application/blogsService";
+import {postsQueryRepository} from "../../repositories/postsQueryRepository";
 
 export const createPostHandler = async (req: Request, res: Response) => {
-    const foundBlog = await blogsService.findById(req.body.blogId);
-    if (!foundBlog) {
-        res.sendStatus(404);
-        return;
-    }
+
     const newPost: PostInputDto = {
         title: req.body.title,
         shortDescription: req.body.shortDescription,
         content: req.body.content,
         blogId: req.body.blogId,
     }
-    const createdPost = await postsService.create(newPost);
+    const createdPostId = await postsService.create(newPost);
+    if (!createdPostId) {
+        res.sendStatus(404)
+        return;
+    }
+    const createdPost = await postsQueryRepository.findById(createdPostId);
     res
         .status(201)
-        .json(mapPostToViewModel(createdPost));
+        .json(createdPost);
 }
