f7215bb7752a891da543f9d9fe93e0c6985aa9dd
Alexander <sasha.kozel252000@gmail.com>
2025-12-05 22:47:25 +0300

    CQRS updates

--- src/posts/application/postsService.ts
+++ src/posts/application/postsService.ts
@@ -1,71 +1,55 @@
-import {WithId} from "mongodb";
+import {ObjectId, WithId} from "mongodb";
 import {Post} from "../types/post";
 import {postsRepository} from "../repositories/postsRepository";
 import {PostInputDto} from "../dto/post-input.dto";
 import {blogsRepository} from "../../blogs/repositories/blogsRepository";
 import {Blog} from "../../blogs/types/blog";
-import {BlogPostsWithPaginator} from "../types/blogPostsWithPaginator";
-import {PostsWithPaginator} from "../types/PostsWithPaginator";
+import {blogsService} from "../../blogs/application/blogsService";
+import {postsCollection} from "../../db/mongo.db";
 
 export const postsService = {
 
-    async findAll(pageSize: number, pageNumber: number, sortDirection: string, sortBy: string): Promise<PostsWithPaginator> {
-        const foundPostsWithPaginator = await postsRepository.findAll(pageSize, pageNumber, sortDirection, sortBy)
-        return {
-            items: foundPostsWithPaginator.items,
-            totalCount: foundPostsWithPaginator.totalCount,
-            pageSize: pageSize,
-            page: pageNumber,
-            pagesCount: Math.ceil(foundPostsWithPaginator.totalCount / pageSize),
-        };
-    },
-
-    async findAllForBlog(id: string, pageNumber: number, pageSize: number, sortBy: string, sortDirection: string): Promise<BlogPostsWithPaginator> {
-
-        const skip = (pageNumber - 1) * pageSize;
-        const limit = pageSize;
-
-        const posts = await postsRepository.findAllForBlog(id, skip, limit, sortBy, sortDirection);
-        return {
-            ...posts,
-            page: pageNumber,
-            pageSize: pageSize,
-            pagesCount: Math.ceil(posts.totalCount / pageSize),
+    async create(post: PostInputDto): Promise<string | null> {
+        const foundBlog = await blogsService.findById(post.blogId);
+        if (!foundBlog) {
+            return null;
         }
-    },
-
-    async findById(id: string): Promise<WithId<Post> | null> {
-        return postsRepository.findById(id);
-    },
-
-    async create(post: PostInputDto): Promise<WithId<Post>> {
-        const foundBlog = await blogsRepository.findById(post.blogId) as Blog
 
         const newPost: Post = {
-            ...post,
+            title: post.title,
+            shortDescription: post.shortDescription,
+            content: post.content,
+            blogId: post.blogId,
             createdAt: new Date().toISOString(),
             blogName: foundBlog.name
         }
 
         return await postsRepository.create(newPost);
+
     },
 
-    async createForBlog(post: PostInputDto): Promise<WithId<Post>> {
-        const foundBlog = await blogsRepository.findById(post.blogId) as Blog
+    async createForBlog(post: PostInputDto): Promise<string | null> {
+        const foundBlog = await blogsService.findById(post.blogId);
+        if (!foundBlog) {
+            return null;
+        }
         const newPost: Post = {
-            ...post,
+            title: post.title,
+            shortDescription: post.shortDescription,
+            content: post.content,
+            blogId: post.blogId,
             blogName: foundBlog.name,
             createdAt: new Date().toISOString(),
         }
         return await postsRepository.create(newPost);
     },
 
-    async update(id: string, dto: PostInputDto): Promise<void> {
-        await postsRepository.update(id, dto);
+    async update(id: string, dto: PostInputDto): Promise<number> {
+        return await postsRepository.update(id, dto);
     },
 
-    async delete(id: string): Promise<void> {
-        await postsRepository.delete(id);
+    async delete(id: string): Promise<number> {
+        return await postsRepository.delete(id);
     }
 
 }
