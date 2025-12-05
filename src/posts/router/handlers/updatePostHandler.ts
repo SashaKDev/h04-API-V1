4bd755aac185f1e1847749f15afa685c866dfb57
Alexander <sasha.kozel252000@gmail.com>
2025-12-05 22:47:37 +0300

    CQRS updates

--- src/posts/router/handlers/updatePostHandler.ts
+++ src/posts/router/handlers/updatePostHandler.ts
@@ -4,24 +4,18 @@ import {postsService} from "../../application/postsService";
 
 export const updatePostHandler = async (req: Request, res: Response) => {
 
-    const post = await postsService.findById(req.params.id);
-    if (!post) {
-        res.sendStatus(404);
-        return;
-    }
-
     const dto: PostInputDto = {
         title: req.body.title,
         shortDescription: req.body.shortDescription,
         content: req.body.content,
         blogId:	req.body.blogId,
     }
-    try {
-        await postsService.update(req.params.id, dto);
-    } catch (err) {
-        res.sendStatus(500);
+    const updateResult: number = await postsService.update(req.params.id, dto);
+    if (!updateResult) {
+        res.sendStatus(404);
         return;
     }
+
     res.sendStatus(204);
 
 }
